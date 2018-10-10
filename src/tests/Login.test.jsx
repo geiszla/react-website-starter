eff05c1649c07705c1b19bb593d076d601e05b19
András Geiszl <geiszla@gmail.com>
2018-10-10 16:15:19 +0100

    Revert "Test"
    This reverts commit ebc202ce889f15f4c6d479ac92d8d29366a573e5.

--- src/tests/Login.test.jsx
+++ src/tests/Login.test.jsx
@@ -1,7 +1,7 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import { SchemaLink } from 'apollo-link-schema';
-import { configure, mount } from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { printSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
@@ -12,8 +12,19 @@ import renderer from 'react-test-renderer';
 
 import schema from '../../server/graphql';
 import App from '../components/App.jsx';
-import { watchFile } from 'fs';
-import wait from 'waait';
+
+// Helper functions
+function testRoute(path) {
+  const tree = renderer.create(
+    <ApolloProvider client={apolloClient}>
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    </ApolloProvider>
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+}
 
 // Setup
 const executableSchema = makeExecutableSchema({
@@ -29,33 +40,8 @@ const apolloClient = new ApolloClient({
 });
 
 configure({ adapter: new Adapter() });
-
-const mockApp = path => (
-  <ApolloProvider client={apolloClient}>
-    <MemoryRouter initialEntries={[path]}>
-      <App />
-    </MemoryRouter>
-  </ApolloProvider>
-);
-
-// Helper functions
-async function createSnapshot(path) {
-  return renderer.create(mockApp(path)).toJSON();
-}
-
-createSnapshot('/');
-
-// Tests
-test('renders the loading component when different routes are requested', async () => {
-  const loginTree = createSnapshot('/');
-  expect(loginTree).toMatchSnapshot();
-
-  const homeTree = createSnapshot('/');
-  expect(homeTree).toMatchSnapshot();
-});
-
-test('logout mutation is called when the logout button is pressed', async () => {
-  const wrapper = mount(mockApp('/'));
-  wrapper.find('App').instance().handleLogout();
-  console.log(wrapper.debug());
+it('renders the loading component when different routes are requested', () => {
+  console.error = undefined;
+  testRoute('/login');
+  testRoute('/');
 });
