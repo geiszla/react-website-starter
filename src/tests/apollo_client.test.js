10f00d729145e9ea6f66b8c1705d0be605205bd9
András Geiszl <geiszla@gmail.com>
2018-10-12 11:46:55 +0100

    Fix apollo_client.js tests

--- src/tests/apollo_client.test.js
+++ src/tests/apollo_client.test.js
@@ -1,4 +1,5 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
 import fetch from 'node-fetch';
 
 import apolloClient from '../apollo_client';
@@ -8,10 +9,15 @@ global.SERVER_URL = 'abc';
 
 describe('Apollo Client', () => {
   it('should export a valid Apollo Client object without SSR', () => {
-    expect(apolloClient(false, undefined, new InMemoryCache())).toMatchSnapshot();
+    expect(apolloClient(false, undefined, new InMemoryCache())).toBeInstanceOf(ApolloClient);
   });
 
   it('should export a valid Apollo Client object with SSR', () => {
-    expect(apolloClient(true, undefined, new InMemoryCache())).toMatchSnapshot();
+    expect(apolloClient(true, undefined, new InMemoryCache())).toBeInstanceOf(ApolloClient);
+  });
+
+  const headers = { accept: 'application/json' };
+  it('should export a valid Apollo Client object with headers', () => {
+    expect(apolloClient(true, headers, new InMemoryCache())).toBeInstanceOf(ApolloClient);
   });
 });
