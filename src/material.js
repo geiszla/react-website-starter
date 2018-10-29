2e00027f4f4f54a6f869bfb95dd3d012b76a5bd2
András Geiszl <geiszla@gmail.com>
2018-10-29 13:25:40 +0000

    Increase test coverage for App.jsx

---
+++ src/material.js
@@ -0,0 +1,14 @@
+import { indigo, white } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default (function createTheme() {
+  return createMuiTheme({
+    palette: {
+      primary: indigo,
+      accent: white
+    },
+    typography: {
+      useNextVariants: true
+    }
+  });
+}());
